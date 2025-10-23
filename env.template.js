28a65670cefcec284236390a1ff8b42b30391826
Andrés Ariza <andres-ariza70@hotmail.com>
2025-10-23 16:14:03 -0500

    first commit

---
+++ env.template.js
@@ -0,0 +1,9 @@
+window.env = {
+  API_URL: "__API_URL__"
+};
+
+/*
+docker build -t api-gateway:latest .
+docker tag frontend andresariz88/arrival-frontend:latest
+docker push andresariz88/arrival-frontend:latest
+*/
